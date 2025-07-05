dfcbeaae49d7bcc91d9f1a5fb7fa6dfb69b48f1c
sahilsharma2184 <sahilsharma2184@gmail.com>
2025-07-06 02:20:57 +0530

    ll

---
+++ Count_Digits_Using_Log.py
@@ -0,0 +1,6 @@
+from math import *
+n = int(input("Enter the number \n"))
+num = n
+count = int(log10(num) + 1)
+print("The original input is", n)
+print("The number of digits in the number ->", count)
