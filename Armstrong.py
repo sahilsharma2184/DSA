f12f3a58e797b5fcedc1bf5a5e7f15fa082cca7b
sahilsharma2184 <sahilsharma2184@gmail.com>
2025-07-06 02:46:54 +0530

    ll

--- Armstrong.py
+++ Armstrong.py
@@ -1,9 +1,10 @@
 n = int(input("Enter the number \n"))
 num = n
 armstrong = 0
+power = len(str(n))
 while num>0:
     last_digit = num % 10
-    armstrong = armstrong + (last_digit ** 3)
+    armstrong = armstrong + (last_digit ** power)
     num = num // 10
 print("Original Input is", n)
 print("Conversion to armstrong gives", armstrong)
@@ -11,3 +12,8 @@ if armstrong == n:
     print("Input given is armstrong number")
 else:
     print("Input given is not armstrong number")
+
+"""
+TC = O(log10 (n)), where n is number. Refer to Count_Digits.py comment
+SC = O(1)
+"""
