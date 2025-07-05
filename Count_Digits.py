e53c1ee09e82243492dbfcc8bd9ebaa44e18662e
sahilsharma2184 <sahilsharma2184@gmail.com>
2025-07-06 02:29:25 +0530

    ll

--- Count_Digits.py
+++ Count_Digits.py
@@ -6,3 +6,13 @@ while num > 0:
     counter = counter+1
     num = num // 10 #updation of the number, if the input n = 123, in this step the it will be updated to 12, and after multiple updations num = 0, and the loop will exit
 print("Number of digits are", counter)
+
+"""
+The time complexity here --->
+Line 4: All the operations are performed inside the while loop
+        Inside the loop the number is getting divided by 10
+        So in anycase where division is done, so see that what is the divisor
+        Whatever will be the divisor, according to that the TC will be calculated
+        Here TC = O(log10 (n)) , where n is the number
+        If the divisor is 2 let's say, then TC = O(log2 (n)) , where n is the number
+"""
