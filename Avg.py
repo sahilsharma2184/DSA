401d4e7926250f7ab338972d3115f415682d61f6
sahilsharma2184 <sahilsharma2184@gmail.com>
2025-07-10 14:18:35 +0530

    ll

---
+++ Avg.py
@@ -0,0 +1,5 @@
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+avg=(a+b)/2 
+print(avg, " is the average of a and b") #4.5  is the average of a and b
+print(type(avg)) #<class 'float'>
